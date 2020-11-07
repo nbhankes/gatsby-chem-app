b0c6c9cb3c7d63a1ab79c358fc8e41de6d71f876
nbankes <nbhankes@gmail.com>
2020-11-06 20:50:17 -0800

    Equation work

--- client/src/assets/js/equation.js
+++ client/src/assets/js/equation.js
@@ -2,7 +2,7 @@ items = [
   {
     cfLabel: "Nitrogen mass per mol",
     num: "14.007",
-    numExp: "1",
+    numExp: "",
     numUnit: "grams",
     numComp: "Nitrogen",
     denom: "1",
@@ -13,7 +13,7 @@ items = [
   {
     cfLabel: "milligrams per gram",
     num: "1000",
-    numExp: "1",
+    numExp: "",
     numUnit: "milligrams",
     numComp: "",
     denom: "1",
@@ -24,7 +24,7 @@ items = [
   {
     cfLabel: "grams of fertilizer per mols Nitrogen",
     num: "45",
-    numExp: "1",
+    numExp: "22",
     numUnit: "milliliters",
     numComp: "fertilizer",
     denom: "2.4",
@@ -34,20 +34,49 @@ items = [
   },
 ]
 
+//Conversion Factor Labels
 const cfLabelArray = items.map(item => item.cfLabel)
+
+//All numerators in an array
 const numArray = items.map(item => item.num)
+
+//All numerator exponents in an array
 const numExpArray = items.map(item => item.numExp)
+
+//All numerator exponents in an array with blank entries removed
+const numExpArrayFiltered = numExpArray.filter(item => item.length > 0)
+
+//All numerator units in an array
 const numUnitArray = items.map(item => item.numUnit)
+
+//All numerator components in an array
 const numCompArray = items.map(item => item.numComp)
+
+//All numerator components in an array with blanlk entries removed
+const numCompArrayFiltered = numCompArray.filter(item => item.length > 0)
+
+//All denominator values in an array
 const denomArray = items.map(item => item.denom)
+
+//All denominator exponent values in an array
 const denomExpArray = items.map(item => item.denomExp)
+
+//All denominator exponent values in an array with blank entries removed
+const denomExpArrayFiltered = denomExpArray.filter(item => item.length > 0)
+
+//All denominator units in an array
 const denomUnitArray = items.map(item => item.denomUnit)
+
+//All denominator components in an array
 const denomCompArray = items.map(item => item.denomComp)
 
-var numAndNumExpArray = []
+//All denominator components in an array with blank entries removed
+const denomCompArrayFiltered = denomCompArray.filter(item => item.length > 0)
 
-for (var i = 0; i < numArray.length; i++) {
-  numAndNumExpArray = numArray[i] * numExpArray[i]
-}
+//Pairing the exponent value to its corresponding numerator value
+const NumExpCount = numExpArrayFiltered.length
 
-console.log(numAndNumExpArray)
+const hasExpValue = item => item > 0
+const ExpIndex = numExpArray.findIndex(hasExpValue)
+
+const product = numArray[ExpIndex] * Math.pow(10, numExpArray[ExpIndex])
