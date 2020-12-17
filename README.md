4377be0f1a66c3973edadb54c46a597e9fc9d701
nbankes <nbhankes@gmail.com>
2020-12-17 05:27:17 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## 🧪 Welcome to my chemistry calculator! 🧪
+
+It's still under construction, and I'm learning as I go, so stop in periodically to check in on my progress. When I have a suitable MVP, I'll share the URL.
+
+### This project performs dimensional analysis calculations using conversion factors. That's scientist lingo for, this app allows you to change units around.
+
+This project is **unique** because the unit conversion software available on the market only allows you to use predetermined conversion factors. For instance, converting miles to kilometers. But this app allows users to add their own conversion factors.
+
+This is an important innovation because scientists often use custom chemical solutions with one-of-a-kind conversion factors. By adding these to the conversion software, users are able to make calculations specific to their laboratory. And since they are stored in the user database, knowledge is not lost as graduate students, interns, and personnel rotate through the laboratory.
+
+### This project is being built with Node, MongoDB, GraphQL, Apollo, and Gatsby JS.
+
+## Getting Started
+
+clone the repo to your local system. Do separate npm installs in the client and server folders. In the terminal, cd to the server folder and run nodemon index.js. Open a new terminal window, cd to the client folder and run gatsby develop.
+
+You will need to provide your own MongoDB URI in the server/index.js file.
---
+++ client/src/components/cfCardAndContainer/cfacquery.js
@@ -0,0 +1,81 @@
+import React from "react"
+import { useQuery, useMutation } from "@apollo/client"
+import { GET_CFACTOR } from "../../apiCalls/queries"
+import { DELETE_CFACTOR, REVERSE_CFACTOR } from "../../apiCalls/mutations"
+
+import { BiShuffle, BiTrash } from "react-icons/bi"
+import "./cfCard.css"
+
+import CFCardDrag from "./cfCardDrag"
+
+function CFacQuery() {
+  const { loading, error, data } = useQuery(GET_CFACTOR)
+
+  const [deleteCFactor] = useMutation(DELETE_CFACTOR, {
+    refetchQueries: [{ query: GET_CFACTOR }],
+  })
+
+  const [reverseCFactor] = useMutation(REVERSE_CFACTOR, {
+    refetchQueries: [{ query: GET_CFACTOR }],
+  })
+
+  if (loading) return "Loading..."
+  if (error) return `Error! ${error.message}`
+
+  return (
+    <>
+      <div className="cfactor-list-container-label">My Conversion Factors</div>
+
+      <div className="individual-cfactor-container">
+        {data.getCFactors.map(cfactor => (
+          <CFCardDrag
+            key={cfactor.id}
+            dataItem={cfactor}
+            dropEffect="link"
+            id={cfactor.id}
+          >
+            <div className="cf-label-card">{cfactor.cfLabel}</div>
+            <div className="cfactor-grid-container">
+              <div className="num-card">{cfactor.num}</div>
+              <div className="num-E-card">e</div>
+              <div className="num-exp-card">{cfactor.numExp}</div>
+              <div className="num-unit-card">{cfactor.numUnit}</div>
+              <div className="num-comp-card">{cfactor.numComp}</div>
+              <div className="division-card"></div>
+              <div className="denom-card">{cfactor.denom}</div>
+              <div className="denom-E-card">e</div>
+              <div className="denom-exp-card">{cfactor.denomExp}</div>
+              <div className="denom-unit-card">{cfactor.denomUnit}</div>
+              <div className="denom-comp-card">{cfactor.denomComp}</div>
+            </div>
+            <div className="cfactor-button-container">
+              <button
+                className="cfactor-card-button delete-button"
+                onClick={() => {
+                  deleteCFactor({
+                    variables: { id: cfactor.id },
+                  })
+                }}
+              >
+                <BiTrash />
+              </button>
+
+              <button
+                className="cfactor-card-button"
+                onClick={() => {
+                  reverseCFactor({
+                    variables: { id: cfactor.id },
+                  })
+                }}
+              >
+                <BiShuffle />
+              </button>
+            </div>
+          </CFCardDrag>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default CFacQuery
